19e85367f6bcbd44858a91944d199dcbc9bbbafa
Jose Maria Payá Castillo <josepaya17@gmail.com>
2020-04-22 15:17:10 +0200

    Updates

--- precache-manifest.4090662424933f7da84942dca693e6aa.js
+++ precache-manifest.4090662424933f7da84942dca693e6aa.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0efd28c5fb34d4bc46490da704b2f999",
+    "revision": "d04bfe6557e501fd47957971bc7660b2",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "a1b630577a9e9b253cd4",
+    "revision": "acaf2a7cbcf4b16867cb",
     "url": "/portfolio/static/css/main.69eda509.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.50c0bb92.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a1b630577a9e9b253cd4",
-    "url": "/portfolio/static/js/main.09bce17e.chunk.js"
+    "revision": "acaf2a7cbcf4b16867cb",
+    "url": "/portfolio/static/js/main.00d0df2c.chunk.js"
   },
   {
     "revision": "6c1d3ad26a766dc494c1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e0ee538db9f1f906feab6b186eb420de.js"
+  "/portfolio/precache-manifest.4090662424933f7da84942dca693e6aa.js"
 );
 
 self.addEventListener('message', (event) => {
