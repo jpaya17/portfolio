8e245c49fc524c7711c60ffaaa7e1e72b514dc88
Jose Maria Payá Castillo <josepaya17@gmail.com>
2020-04-23 09:29:20 +0200

    Updates

---
+++ precache-manifest.7851c86ede5453fdd388ef43f27de025.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cefba22efbbbce5ac720061e65b77915",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1baf18fee51a529441fd",
+    "url": "/portfolio/static/js/2.4947e19c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.4947e19c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b685dc241e6125b2f4e",
+    "url": "/portfolio/static/js/main.c4c4d327.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.677962b2f31f809b6afd35c2c97c856b.js"
+  "/portfolio/precache-manifest.7851c86ede5453fdd388ef43f27de025.js"
 );
 
 self.addEventListener('message', (event) => {
