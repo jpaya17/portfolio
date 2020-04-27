fc4fddc4401bb5d2a3576ee862651dfcefa16fb9
Jose Maria Payá Castillo <josepaya17@gmail.com>
2020-04-27 12:57:27 +0200

    Add Github and Twitter social icons

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -4,10 +4,16 @@ function SocialIcons() {
   return (
     <div className="social-icons">
       <a href="https://www.linkedin.com/in/jpayacastillo" target="_blank">
-        <img className="social-icon-pic" src="/portfolio/img/linkedin.png" alt="linkedin"/>
+        <img className="social-icon-pic" src="/portfolio/img/linkedin.png" alt="LinkedIn Profile"/>
+      </a>
+      <a href="https://github.com/jpaya17" target="_blank">
+        <img className="social-icon-pic" src="/portfolio/img/github.png" alt="Github Profile"/>
+      </a>
+      <a href="https://twitter.com/JoseMpaya" target="_blank">
+        <img className="social-icon-pic" src="/portfolio/img/twitter.png" alt="Twitter Profile"/>
       </a>
       <a href="mailto:josepaya17@gmail.com" target="_blank">
-        <img className="social-icon-pic mail" src="/portfolio/img/mail.png" alt="mail" />
+        <img className="social-icon-pic mail" src="/portfolio/img/mail.png" alt="Send Email" />
       </a>
       {style}
     </div>
